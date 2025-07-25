b8b3fa3d12406a473b6f5bbf8bdd7f0a322adc95
morphalex90 <piero.nanni@gmail.com>
2025-07-25 12:46:52 +0100

    set selfHosted

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import PlausibleProvider from 'next-plausible';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PlausibleProvider domain="onlydogs.icu" customDomain="plausible.morpheus90.com">
+    <PlausibleProvider domain="onlydogs.icu" customDomain="plausible.morpheus90.com" selfHosted={true}>
       <Context>
         <Component {...pageProps} />
       </Context>
