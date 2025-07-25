e72e13b428ece12ac99d1b2831bbe46275b0054e
morphalex90 <piero.nanni@gmail.com>
2025-07-25 12:50:12 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import PlausibleProvider from 'next-plausible';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PlausibleProvider domain="onlydogs.icu" customDomain="plausible.morpheus90.com" selfHosted={true}>
+    <PlausibleProvider domain="onlydogs.icu" selfHosted={true} trackOutboundLinks={true} scriptProps={{ src: 'https://plausible.morpheus90.com/js/script.js' }}>
       <Context>
         <Component {...pageProps} />
       </Context>
