c8ee532b5d0af63e4a6df5769a45580aa2a19e57
morphalex90 <piero.nanni@gmail.com>
2025-07-25 12:42:33 +0100

    add plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/css/index.scss';
 import Context from '@/contex/BreedContext';
 import type { AppProps } from "next/app";
+import PlausibleProvider from 'next-plausible';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Context>
-      <Component {...pageProps} />
-    </Context>
+    <PlausibleProvider domain="onlydogs.icu" customDomain="plausible.morpheus90.com">
+      <Context>
+        <Component {...pageProps} />
+      </Context>
+    </PlausibleProvider>
   )
 }
