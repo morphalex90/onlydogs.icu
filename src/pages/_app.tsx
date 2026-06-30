026f5f90d8d5bdd879e46d2467da0c90c243c865
morphalex90 <piero.nanni@gmail.com>
2026-06-30 17:48:43 +0100

    updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,7 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <PlausibleProvider
-            domain="onlydogs.icu"
-            selfHosted={true}
-            trackOutboundLinks={true}
-            scriptProps={{ src: 'https://plausible.morpheus90.com/js/script.js' }}
-        >
+        <PlausibleProvider src="https://plausible.morpheus90.com/js/pa-eRZWW3v3RU7snouQtGM8F.js">
             <Context>
                 <Component {...pageProps} />
             </Context>
